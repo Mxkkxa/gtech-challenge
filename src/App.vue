e43ff69429920dbf8d246f1e6e4ea3d5deccc1de
Mxkkxa <36981992+Mxkkxa@users.noreply.github.com>
2021-01-08 17:58:57 +0300

    i18n integration

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,20 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+      <HelloI18n/>
     <router-view/>
   </div>
 </template>
+
+<script>
+import HelloI18n from "./components/HelloI18n";
+export default {
+    name: 'app',
+  components: {
+    HelloI18n
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
