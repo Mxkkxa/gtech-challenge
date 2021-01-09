f6e05a2365bc348c8426879abed1958f79730bd8
Mxkkxa <36981992+Mxkkxa@users.noreply.github.com>
2021-01-09 18:32:13 +0300

    fix errors, logic for movie and series fetching and serving

--- src/store/index.js
+++ src/store/index.js
@@ -13,59 +13,85 @@ export default new Vuex.Store({
     popularMovies: {},
     popularTVShows: {},
     movieAndTVBucket: {
-      fillCount: 2,
+      fillCount: 0,
       results: []
     },
     loadedMovieAndTVShows: [],
-    currentPage: '',
+    currentPage: 1,
     sortType: '',
     searchTerm: '',
     selectedMovieOrTVId: '',
 
   },
   getters: {
-    getFetchMoviesUrl: state => `${state.moviesUrl}${state.apiKey}`,
-    getFetchTVShowsUrl: state => `${state.TVShowsUrl}${state.apiKey}`
+    getFetchMoviesUrl: state => `${state.moviesUrl}${state.apiKey}&page=${state.currentPage}`,
+    getFetchTVShowsUrl: state => `${state.TVShowsUrl}${state.apiKey}&page=${state.currentPage}`
   },
   mutations: {
-    SET_POPULAR_MOVIES({ state }, payload) {
+    SET_POPULAR_MOVIES(state, payload) {
       state.popularMovies = payload
     },
-    SET_POPULAR_TV_SHOWS({ state }, payload) {
+    SET_POPULAR_TV_SHOWS(state, payload) {
       state.popularTVShows = payload
     },
-    SET_CURRENT_PAGE({ state }, payload) {
+    SET_CURRENT_PAGE(state, payload) {
       state.currentPage = payload
     },
-    SET_SORT_TYPE({ state }, payload) {
+    INCREASE_CURRENT_PAGE(state) {
+      state.currentPage = state.currentPage + 1
+    },
+    SET_SORT_TYPE(state, payload) {
       state.sortType = payload
     },
-    SET_SEARCH_TERM({ state }, payload) {
+    SET_SEARCH_TERM(state, payload) {
       state.searchTerm = payload
     },
-    SET_SELECTED_MOVIE_OR_TV_ID({ state }, payload) {
+    SET_SELECTED_MOVIE_OR_TV_ID(state, payload) {
       state.selectedMovieOrTVId = payload
     },
-    SET_MOVIE_AND_TV_BUCKET({ state }, payload) {
+    SET_MOVIE_AND_TV_BUCKET(state, payload) {
       state.movieAndTVBucket = payload
     },
-    ADD_LOADED_MOVIE_AND_TV_SHOWS({ state }, payload) {
+    ADD_LOADED_MOVIE_AND_TV_SHOWS(state, payload) {
       state.loadedMovieAndTVShows.push(payload)
     },
   },
   actions: {
-    _fetchMovies({ getters }) {
-      axios.get(getters.getFetchMoviesUrl)
+    _fetchMovies({ getters, commit }) {
+      return axios.get(getters.getFetchMoviesUrl)
         .then(response => {
-          console.log(response)
+          commit('SET_POPULAR_MOVIES', response.data)
+          return response.data
         })
     },
-    _fetchTVShows({ getters }) {
-      axios.get(getters.getFetchTVShowsUrl)
+    _fetchTVShows({ getters, commit }) {
+      return axios.get(getters.getFetchTVShowsUrl)
         .then(response => {
-          console.log(response)
+          commit('SET_POPULAR_TV_SHOWS', response.data)
+          return response.data
         })
-    }
+    },
+    // todo mixture of 2 arrays
+    // fetches 2 shows
+    // increases page count
+    // concats shows
+    // sets bucket size
+    // assigns concatted shows to bucket
+    // returns the value
+    fillMoviesAndTVBucket({ dispatch, state, commit }) {
+      if (state.movieAndTVBucket.fillCount <= 0) {
+        return Promise.all([dispatch('_fetchMovies'), dispatch('_fetchTVShows')]).then(() => {
+          commit('INCREASE_CURRENT_PAGE')
+          const combinedShows = [].concat(state.popularMovies.results, state.popularTVShows.results)
+          state.movieAndTVBucket.fillCount = 1
+          state.movieAndTVBucket.results = combinedShows
+          return state.movieAndTVBucket.results.slice(0, 20)
+        })
+      } else {
+        state.movieAndTVBucket.fillCount--
+        return state.movieAndTVBucket.results.slice(20, 41)
+      }
+    },
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,27 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <button @click="loadm">fetch shows</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     HelloWorld
+  },
+  async beforeMount() {
+    // console.log(await this.$store.dispatch("fillMoviesAndTVBucket"));
+  },
+  methods: {
+    async loadm() {
+      console.log(await this.$store.dispatch("fillMoviesAndTVBucket"));
+    }
   }
-}
-
+};
 </script>
