63209b13ff9954557e29d01a948f0a44fb41a7a2
Mxkkxa <36981992+Mxkkxa@users.noreply.github.com>
2021-01-10 14:46:06 +0300

    added Vuetify

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import i18n from './i18n'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
@@ -10,6 +11,7 @@ new Vue({
   router,
   store,
   i18n,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
 
